b03e41e3e5971dc1b3d0a13780e5522cfa865c9c
yvens <yvensre@gmail.com>
2020-11-21 22:16:18 +0100

    Add "Nuggets!" upgrade to Mines

--- Assets/Scripts/MineHarvest.cs
+++ Assets/Scripts/MineHarvest.cs
@@ -8,6 +8,7 @@ public class MineHarvest : Upgradable
     [Header("Mining Properties")]
     [SerializeField] private int mineHarvestPerTick = 1;
     [SerializeField] private float mineHarvestTick = 5.0f;
+    [SerializeField] private float nuggetsChance = 0.0f;
 
     private IEnumerator _harvestCorountine;
 
@@ -22,28 +23,32 @@ public class MineHarvest : Upgradable
         while (true)
         {
             yield return new WaitForSeconds(mineHarvestTick);
-            GameLogic.Instance.IncrementMinerals(mineHarvestPerTick);
+            int nuggetsIncrement = Random.Range(0.0f, 100.0f) < nuggetsChance ? 1 : 0;
+            GameLogic.Instance.IncrementMinerals(mineHarvestPerTick + nuggetsIncrement);
         }
     }
 
     public void UnlockUpgrade(UpgradeInfo info)
     {
-        //Tier 1
-        if (info.name.Equals("Mine Faster"))
+        switch (info.name)
         {
-            mineHarvestTick = 3.0f;
-        } else if (info.name.Equals("Mine Harder"))
-        {
-            mineHarvestPerTick = 2;
-        }
-        
-        //Tier 2
-        if (info.name.Equals("Mine Even Faster"))
-        {
-            mineHarvestTick = 2.0f;
-        } else if (info.name.Equals("Mine Even Harder"))
-        {
-            mineHarvestPerTick = 3;
+            //Tier 1
+            case "Mine Faster": 
+                mineHarvestTick = 3.0f;
+                break;
+            case "Mine Harder":
+                mineHarvestPerTick = 2;
+                break;
+            //Tier 2
+            case "Mine Even Faster": 
+                mineHarvestTick = 2.0f;
+                break;
+            case "Mine Even Harder":
+                mineHarvestPerTick = 3;
+                break;
+            case "Nuggets!":
+                nuggetsChance = 5.0f;
+                break;
         }
     }
 }
