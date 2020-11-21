29dfe7a66a8d235d1a0a2a9dca851d029772e50d
yvens <yvensre@gmail.com>
2020-11-21 17:26:34 +0100

    Add average FPS counter
    Closes #24

--- Assets/Scripts/Utils/FPSCounter.cs
+++ Assets/Scripts/Utils/FPSCounter.cs
@@ -1,19 +1,33 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Text))]
 public class FPSCounter : MonoBehaviour
 {
+    [SerializeField]
     public Text text;
-
+    
+    [SerializeField]
+    private int averageSample;
+    private float[] _averageValues;
+    private int _averageIndex;
+    
     public void Awake()
     {
         text = GetComponent<Text>();
+        _averageValues = new float[averageSample];
+        _averageIndex = 0;
+        _averageValues[_averageIndex] = 0;
     }
 
     public void Update()
     {
+        _averageValues[_averageIndex] = Time.deltaTime;
+        _averageIndex = (++_averageIndex) % averageSample;
         var fps = 1.0f / Time.deltaTime;
-        text.text = fps.ToString("0.0");
+        var averageFps = 1.0f / (_averageValues.Sum(value => value) / averageSample);
+        text.text = "FPS: " + fps.ToString("0.0") + " AVG: " + averageFps.ToString("0.0");
     }
 }
