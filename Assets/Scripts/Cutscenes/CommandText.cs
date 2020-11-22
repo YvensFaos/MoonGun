fa6750de908921f4f6da7a3ced3dff94e0d3263f
yvens <yvensre@gmail.com>
2020-11-22 20:14:07 +0100

    Create introduction timeline

---
+++ Assets/Scripts/Cutscenes/CommandText.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandText : MonoBehaviour
+{
+    [SerializeField] private Text textToAnimate;
+    [SerializeField] private float animateTimer;
+    
+    public void ClearText()
+    {
+        textToAnimate.text = "";
+    }
+
+    public void SetTimer(float timer)
+    {
+        animateTimer = timer;
+    }
+
+    public void SetText(string text)
+    {
+        ClearText();
+        textToAnimate.DOText(text, animateTimer, true, ScrambleMode.Lowercase);
+    }
+}
---
+++ Assets/Scripts/Utils/ForceTimelinePlay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class ForceTimelinePlay : MonoBehaviour
+{
+    private PlayableDirector _playableDirector;
+
+    public void Awake()
+    {
+        _playableDirector = GetComponent<PlayableDirector>();
+    }
+
+    public void Start()
+    {
+        _playableDirector.Play();
+    }
+}
