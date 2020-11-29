e88c6f84c5d199f7bb133c4a22c5fa579284bccf
yvens <yvensre@gmail.com>
2020-11-29 22:21:13 +0100

    Change to combat song when in combat mode
    Close #30

---
+++ Assets/Scripts/Utils/ChangeSongTo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ChangeSongTo : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    [SerializeField] private AudioClip baseSong;
+    [SerializeField] private AudioClip combatSong;
+    
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ChangeToCombatSong()
+    {
+        PlaySong(combatSong);
+    }
+    
+    public void ChangeToBaseSong()
+    {
+        PlaySong(baseSong);
+    }
+
+    private void PlaySong(AudioClip song)
+    {
+        _audioSource.clip = song;
+        _audioSource.Stop();
+        _audioSource.Play();
+    }
+}
