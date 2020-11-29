105522d0a5a237ec721d80fee7091150988daa0b
yvens <yvensre@gmail.com>
2020-11-29 18:55:14 +0100

    Create game intro

---
+++ Assets/Scripts/Intro/LoadScene.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad;
+    [SerializeField] private bool allowSceneToChange;
+
+    private void Awake()
+    {
+        allowSceneToChange = false;
+    }
+
+    public void ChangeSceneToLoad(string otherSceneToLoad)
+    {
+        sceneToLoad = otherSceneToLoad;
+    }
+    
+    public void StartAsyncLoading()
+    {
+        StartCoroutine(LoadSceneCoroutine());
+    }
+
+    public void AllowSceneToChange()
+    {
+        allowSceneToChange = true;
+    }
+
+    IEnumerator LoadSceneCoroutine()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        asyncOperation.allowSceneActivation = false;
+        
+        while (!asyncOperation.isDone)
+        {
+            if (asyncOperation.progress >= 0.9f)
+            {
+                yield return new WaitUntil(()=>
+                {
+                     return allowSceneToChange;
+                });
+                asyncOperation.allowSceneActivation = true;
+                allowSceneToChange = false;
+            }
+
+            yield return null;
+        }
+    }
+}
